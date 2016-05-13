02f78b695c60917b3208a9ba81f8c87e6cdd2c80
Endin <endin@platformasoft.ru>
2016-05-13 18:28:20 +0300

    add console support

--- bin/console.php
+++ bin/console.php
@@ -1,1 +1,17 @@
 <?php
+
+use Symfony\Component\Console\Application;
+use Interop\Container\ContainerInterface;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
+$application = new Application('Application console');
+
+$commands = $container->get('config')['console']['commands'];
+foreach ($commands as $command) {
+    $application->add($container->get($command));
+}
+
+$application->run();
